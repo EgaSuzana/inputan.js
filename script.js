ef7bcd61c7d7c321085e036e4cd695c683a9bb94
EgaSuzana <142204907+EgaSuzana@users.noreply.github.com>
2023-11-21 16:25:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript is not strictly necessary for this effect, but included for future enhancements
+
+// You can add more functionality here if needed
+function validateForm() {
+    // Your validation logic goes here
+    alert("Formulir berhasil dikirim!");
+    document.getElementById("myForm").reset();
+  }
+  
